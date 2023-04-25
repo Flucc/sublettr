c19bfa1b4715192ab4b58e30621c3596f0528b3d
Stefan Redinger-Ramos <stefanredram@gmail.com>
2023-04-24 22:23:59 -0400

    Initial commit

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://kzqeeqcvryjxueiqowzx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cWVlcWN2cnlqeHVlaXFvd3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzMTY4MjEsImV4cCI6MTk5Nzg5MjgyMX0._aGPfZzUhywRwNo31IFhXTye8hBTsu03IML5Nvmx8qo')
---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let data;
+    let { countries } = data;
+    $: ({ countries } = data);
+  </script>
+<ul>
+	bruh
+</ul>
+  <ul>
+    {#each countries as country}
+      <li>{country.name}</li>
+    {/each}
+  </ul>
